6ebc104a8f0306b5c67bbc164af2283051bccf04
Thomas Colin <79255127+ThomasColin1@users.noreply.github.com>
2022-06-07 00:17:33 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import feuille1 from './media/feuille1.png'
+import feuille2 from './media/feuille2.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <img src={feuille1} alt="feuille1" id="feuille1"></img>
+      <img src={feuille2} alt="feuille2" id="feuille2"></img>
+
+      <button class="c0 l0"><p>A</p></button>
+      <button class="c1 l0"><p>Z</p></button>
+      <button class="c2 l0"><p>E</p></button>
+      <button class="c3 l0"><p>R</p></button>
+      <button class="c0 l1"><p>Q</p></button>
+      <button class="c1 l1"><p>S</p></button>
+      <button class="c2 l1"><p>D</p></button>
+      <button class="c3 l1"><p>F</p></button>
+
+      
+
+
     </div>
   );
 }
